6d174e455a7aa348923204a91d5e02e8c64f5ccb
Kinga <k.wierchomska@gmail.com>
2021-04-11 15:12:07 +0200

    FOODSHARIN-21 functional components and bootstrap

--- services/frontend/src/components/Message.jsx
+++ services/frontend/src/components/Message.jsx
@@ -1,25 +1,24 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-
-const Message = props => {
-  return (
-    <div className="container">
-      <section data-testid="message">
-        <div className={`notification is-${props.messageType}`}>
-          <button
-            className="delete"
-            onClick={() => {
-              props.removeMessage();
-            }}
-          />
-          {/* message-text is for testing */}
-          <span className="message-text">{props.messageText}</span>
+function Message(props) {
+    return (
+        <div className="container">
+            <section data-testid="message">
+                <div className={`notification is-${props.messageType}`}>
+                    <button
+                      className="delete"
+                      onClick={() => {
+                            props.removeMessage();
+                        }}
+                    />
+                    {/* message-text is for testing */}
+                    <span className="message-text">{props.messageText}</span>
+                </div>
+            </section>
         </div>
-      </section>
-    </div>
-  );
-};
+    );
+}
 
 Message.propTypes = {
   messageType: PropTypes.string.isRequired,
