be54099ccd6800a3af957e844b2b671a488d8e32
Michał Kurleto <michal.kurleto@gmail.com>
2021-04-12 19:22:50 +0200

    FOODSHARIN-4 fix bug

--- services/frontend/src/components/TimeOut.jsx
+++ services/frontend/src/components/TimeOut.jsx
@@ -4,6 +4,8 @@ import { history } from "../index"
 const TimeOut = ({ createMessage }) => {
 
     useEffect(() => {
+        localStorage.removeItem("accessToken");
+        console.log("Elapsed token removed!")
         createMessage("danger", "Session elapsed. You need to log in again")
         history.push("/login");
     }, []);
