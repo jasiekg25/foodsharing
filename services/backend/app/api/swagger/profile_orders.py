8cfbf126c00021d94e3fed5833c075ecba65dabf
jasiek <jan.karolgargas@dathena.io>
2021-05-18 00:13:52 +0200

    add user's orders endpoint

--- services/backend/app/api/swagger/profile_orders.py
+++ services/backend/app/api/swagger/profile_orders.py
@@ -8,7 +8,7 @@ from app import logger
 from app.api.models.offer import Offer
 from app.api.models.order import Order
 
-orders_namespace = Namespace("orders")
+orders_namespace = Namespace("profile_orders")
 offers_namespace = Namespace("offers")
 
 # doing this add description to Swagger Doc
@@ -37,52 +37,22 @@ offer = offers_namespace.model(
 )
 
 
-parser = reqparse.RequestParser()
-
 class Orders(Resource):
-    @orders_namespace.expect(parser)
+    @auth_required
     @orders_namespace.marshal_with(order)
     def get(self):
         """Returns all orders"""
         logger.info("Orders.get()")
         try:
-            orders = Order.get_all_orders()
-            return [order.to_dict() for order in orders], 200
+            user_id = current_user().id
+            orders =  Order.get_orders_of_user(user_id=user_id)
 
+
+            return [order.to_dict() for order in orders], 200
         except Exception as e:
             logger.exception("Orders.get(): %s", str(e))
             return "Couldn't load orders", 500
 
-    @auth_required
-    @orders_namespace.expect(order, validate=True)
-    def post(self):
-        """Place new order"""
-        logger.info("Orders.post() request_body: %s", str(request.get_json()))
-        try:
-            content = request.get_json()
-            offer_from_order = Offer.get_offer_by_id(content['offer_id'])
-            offer_dict = offer_from_order.to_dict()
 
-            user_id = current_user().id
-
-            if offer_dict['portions_number'] - offer_dict['used_portions'] < content['portions']:
-                return "Not enough portions", 400
-
-            if not offer_dict['active']:
-                return "Offer not active any more", 400
-
-            if offer_dict['offer_expiry'] < datetime.now():
-                return "Offer expired", 400
-
-            new_order_portion = content['portions'] + offer_dict['used_portions']
-            Offer.update_used_portions(content['offer_id'], new_order_portion)
-
-            Order.add_order(user_id, content['offer_id'], datetime.now(), 1)
-
-            return "Order placed", 201
-
-        except Exception as e:
-            logger.exception("Orders.post(): %s", str(e))
-            return "Couldn't make order", 500
 
 orders_namespace.add_resource(Orders, "")
