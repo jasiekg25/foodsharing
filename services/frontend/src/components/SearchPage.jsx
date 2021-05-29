e3247c59e3c8b621607723d1da8b17e03be23c11
Kinga <k.wierchomska@gmail.com>
2021-05-29 10:59:02 +0200

    FOODSHARIN-88 search component

--- services/frontend/src/components/SearchPage.jsx
+++ services/frontend/src/components/SearchPage.jsx
@@ -4,74 +4,24 @@ import { Row, Container, Button } from "react-bootstrap";
 import Tag from "./tags/Tag";
 import Offers from "./Offers";
 import {Redirect} from "react-router-dom";
-
-
-const mockTags = [
-  {
-    id: 1,
-    tag_name: "Free",
-    selected: false,
-  },
-  {
-    id: 2,
-    tag_name: "Vegan",
-    selected: false,
-  },
-  {
-    id: 3,
-    tag_name: "Vegetarian",
-    selected: false,
-  },
-  {
-    id: 4,
-    tag_name: "GlutenFree",
-    selected: false,
-  },
-  {
-    id: 5,
-    tag_name: "Diary",
-    selected: false,
-  },
-  {
-    id: 6,
-    tag_name: "DiaryFree",
-    selected: false,
-  },
-  {
-    id: 7,
-    tag_name: "Sweet",
-    selected: false,
-  },
-  {
-    id: 8,
-    tag_name: "Meat",
-    selected: false,
-  },
-  {
-    id: 9,
-    tag_name: "Soup",
-    selected: false,
-  },
-  {
-    id: 10,
-    tag_name: "Vegetables",
-    selected: false,
-  },
-];
+import api from "../api";
 
 const SearchPage = ({ isLoggedIn }) => {
   const [tags, setTags] = useState([]);
-  const [tagSearchVisible, setTagSearchVisible] = useState(false);
 
   useEffect(() => {
-    // TODO: when there is an endpoint for this add "selected" field
-    // api
-    //   .getTags()
-    //   .then((res) => setTags(res.data))
-    //   .catch((err) => console.log("Could not get any tags " + err.message));
-
-    setTags(mockTags);
-  }, []);
+    api.getTags()
+        .then((res) => {
+          let tagsData = res.data;
+          for(let tag of tagsData){
+            tag['selected'] = false;
+          }
+          setTags(tagsData);
+        })
+        .catch((err) => {
+          console.log("Could not get any tags " + err.message);
+        })
+  }, [])
 
   const onTagToggle = (tag) => {
     const index = tags.findIndex((el) => el.id === tag.id);
@@ -87,26 +37,22 @@ const SearchPage = ({ isLoggedIn }) => {
 
   return (
     <div>
-      {!tagSearchVisible && (
-        <Button onClick={() => setTagSearchVisible(true)}>Add Tags</Button>
-      )}
       <Container>
-        <Row>
-          {tags
-            .filter((tag) => tag.selected)
-            .map((tag) => {
-              return <Tag key={tag.id} tag={tag} toggle={onTagToggle} />;
-            })}
-        </Row>
+        {/*<Row>*/}
+        {/*  {tags*/}
+        {/*    .filter((tag) => tag.selected)*/}
+        {/*    .map((tag) => {*/}
+        {/*      return <Tag key={tag.id} tag={tag} toggle={onTagToggle} />;*/}
+        {/*    })}*/}
+        {/*</Row>*/}
       </Container>
-      {tagSearchVisible && (
         <TagSearch
           tags={tags}
           onTagToggle={onTagToggle}
-          close={() => setTagSearchVisible(false)}
-          containerStyle="col-md-3 search-container"
+          searchButton={true}
+          containerStyle="col-md-6 search-container"
         />
-      )}
+      }
 
       <Offers />
     </div>
