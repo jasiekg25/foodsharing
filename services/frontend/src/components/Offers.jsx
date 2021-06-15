e5b118d8290a85ce9baf35215c13dfa5bd3bb8cc
MateuszNov <mmastalerczyk@novelaneuro.com>
2021-06-15 21:26:48 +0200

    add infinite scroll

--- services/frontend/src/components/Offers.jsx
+++ services/frontend/src/components/Offers.jsx
@@ -7,8 +7,9 @@ import 'react-toastify/dist/ReactToastify.css';
 import {useForm} from "react-hook-form";
 import {yupResolver} from "@hookform/resolvers/yup";
 import * as yup from "yup";
-import {AlarmFill, BucketFill, ClockFill, GeoAltFill, TagFill} from "react-bootstrap-icons";
+import {AlarmFill, BucketFill, ClockFill, DistributeHorizontal, GeoAltFill, TagFill} from "react-bootstrap-icons";
 import { history } from "../index";
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 const portions = {
     name: "portions",
@@ -32,17 +33,19 @@ function Offers() {
     });
 
     const [offers, setOffers] = useState([]);
-    const [chosenOffer, setChosenOffer] = useState({})
+    const [chosenOffer, setChosenOffer] = useState({});
     const [showModal, setShowModal] = useState(false);
-
+    let [pageCount, setCount] = React.useState(0);
+    let [isNext, isNextFunc] = React.useState(false);
     useEffect(() => {
+        if (pageCount !== -1) isNextFunc(true);
         getOffers();
-    }, [])
+    }, [pageCount])
 
     const getOffers = () => {
-        api.getOffers()
+        api.getOffers() //TODO send page, etc
             .then((res) => {
-                setOffers(res.data);
+                setOffers(offers.concat(res.data));
             })
             .catch((err) => {
                 console.log("Could not get any offers " + err.message);
@@ -78,58 +81,70 @@ function Offers() {
     }
 
     return (
-        offers.map((offer) => {
-            return (
-                <div key={offer.id}>
-                    <Row className="offers-container">
-                            <Card className="offer-card">
-                                <Card.Body>
-                                    <Card.Title>{offer.name}</Card.Title>
-                                    <Card.Text>
-                                        {offer.description}
-                                    </Card.Text>
-                                    <ListGroup className="list-group-flush">
-                                        <ListGroupItem> <GeoAltFill size={15}/> <strong> Pick-up localization: </strong> {offer.pickup_localization}</ListGroupItem>
-                                        <ListGroupItem> <ClockFill size={15}/> <strong> Pick-up times: </strong> {offer.pickup_times}</ListGroupItem>
-                                        <ListGroupItem> <AlarmFill size={15}/> <strong> Expire date: </strong> {offer.offer_expiry}</ListGroupItem>
-                                        <ListGroupItem> <BucketFill size={15}/>  <strong>Remaining portions: </strong> {offer.portions_number - offer.used_portions} </ListGroupItem>
-                                        <ListGroupItem> <TagFill size={15}/> <strong>Tags: </strong> {Array.from(offer.tags).map((tag, index) => {
-                                            return (<small key={index}>{tag}, </small>);
-                                        })}
-                                        </ListGroupItem>
-                                    </ListGroup>
-                                    <div>
-                                        <Card.Text className="view-profile-button"> <Button className="view-button" variant="secondary" onClick={(e) => handleShowUserProfile(offer.user_id)}>View user profile </Button> {offer.user_name}</Card.Text>
-                                        <Button className="order-button" variant="success" onClick={(e) => handleShow(offer)}>Make order</Button>
-                                    </div>
-                                    <Modal className="portions-modal" show={showModal} onHide={handleClose} backdrop="static">
-                                        <Modal.Header closeButton>
-                                            <Modal.Title>Choose number of portions</Modal.Title>
-                                        </Modal.Header>
-                                        <Modal.Body>
-                                            <form onSubmit={handleSubmit((data) => {orderMeal(data)})}>
-                                                <Form.Control min="0" max={chosenOffer.portions_number - chosenOffer.used_portions} defaultValue='0' {...register(portions.name)} type={portions.type}/>
-                                                {errors[portions.name] && (
-                                                    <p className="error login-error">{errors[portions.name].message}</p>
-                                                )}
-                                                <Modal.Footer>
-                                                    <Button className="order-button" variant="success" type="submit">
-                                                        Order
-                                                    </Button>
-                                                    <Button className="cancel-order-button" variant="dark" onClick={handleClose}>
-                                                        Cancel
-                                                    </Button>
-                                                </Modal.Footer>
-                                            </form>
-                                        </Modal.Body>
-                                    </Modal>
-                                </Card.Body>
-                            </Card>
-                    </Row>
+        <div>
+            <InfiniteScroll
+                dataLength={offers.length} //This is important field to render the next data
+                next={getOffers}
+                hasMore={true}
+                height='1000px'
+                loader={<h4>Loading...</h4>}
+                >
+                <div>
+                    {offers.map((offer) => {
+                        return (
+                            <div key={offer.id}>
+                                <Row className="offers-container">
+                                        <Card className="offer-card">
+                                            <Card.Body>
+                                                <Card.Title>{offer.name}</Card.Title>
+                                                <Card.Text>
+                                                    {offer.description}
+                                                </Card.Text>
+                                                <ListGroup className="list-group-flush">
+                                                    <ListGroupItem> <GeoAltFill size={15}/> <strong> Pick-up localization: </strong> {offer.pickup_localization}</ListGroupItem>
+                                                    <ListGroupItem> <ClockFill size={15}/> <strong> Pick-up times: </strong> {offer.pickup_times}</ListGroupItem>
+                                                    <ListGroupItem> <AlarmFill size={15}/> <strong> Expire date: </strong> {offer.offer_expiry}</ListGroupItem>
+                                                    <ListGroupItem> <BucketFill size={15}/>  <strong>Remaining portions: </strong> {offer.portions_number - offer.used_portions} </ListGroupItem>
+                                                    <ListGroupItem> <TagFill size={15}/> <strong>Tags: </strong> {Array.from(offer.tags).map((tag, index) => {
+                                                        return (<small key={index}>{tag}, </small>);
+                                                    })}
+                                                    </ListGroupItem>
+                                                </ListGroup>
+                                                <div>
+                                                    <Card.Text className="view-profile-button"> <Button className="view-button" variant="secondary" onClick={(e) => handleShowUserProfile(offer.user_id)}>View user profile </Button> {offer.user_name}</Card.Text>
+                                                    <Button className="order-button" variant="success" onClick={(e) => handleShow(offer)}>Make order</Button>
+                                                </div>
+                                                <Modal className="portions-modal" show={showModal} onHide={handleClose} backdrop="static">
+                                                    <Modal.Header closeButton>
+                                                        <Modal.Title>Choose number of portions</Modal.Title>
+                                                    </Modal.Header>
+                                                    <Modal.Body>
+                                                        <form onSubmit={handleSubmit((data) => {orderMeal(data)})}>
+                                                            <Form.Control min="0" max={chosenOffer.portions_number - chosenOffer.used_portions} defaultValue='0' {...register(portions.name)} type={portions.type}/>
+                                                            {errors[portions.name] && (
+                                                                <p className="error login-error">{errors[portions.name].message}</p>
+                                                            )}
+                                                            <Modal.Footer>
+                                                                <Button className="order-button" variant="success" type="submit">
+                                                                    Order
+                                                                </Button>
+                                                                <Button className="cancel-order-button" variant="dark" onClick={handleClose}>
+                                                                    Cancel
+                                                                </Button>
+                                                            </Modal.Footer>
+                                                        </form>
+                                                    </Modal.Body>
+                                                </Modal>
+                                            </Card.Body>
+                                        </Card>
+                                </Row>
+                            </div>
+                        )
+                    })}
                 </div>
-            )
-        })
-    );
+            </InfiniteScroll>
+        </div>
+    )
 }
 
 export default Offers;
