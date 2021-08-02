e19ab78b15b0f09b520984e55dd6b2c06a796d03
Mateusz <mmastalerczyk@gmail.com>
2021-08-02 23:38:44 +0100

    add saving notifs to db, endpoint for db, send notifs on cancel and pick

--- services/backend/app/api/models/orders.py
+++ services/backend/app/api/models/orders.py
@@ -56,7 +56,6 @@ class Orders(db.Model):
 
         db.session.commit()
 
-
     @staticmethod
     def get_all_orders():
         return Orders.query.all()
@@ -64,4 +63,8 @@ class Orders(db.Model):
     @staticmethod
     def get_orders_of_user(user_id):
         return Orders.query.filter_by(user_id=user_id).order_by(Orders.is_canceled, Orders.is_picked)
+
+    @staticmethod
+    def get_order_by_id(order_id):
+        return Orders.query.filter_by(id=order_id).first()
 
