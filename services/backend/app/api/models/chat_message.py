565012f984d07248a13227447d74a60c8ad44c62
jasiek <jan.karolgargas@dathena.io>
2021-08-02 19:39:09 +0200

    table 'chat_room' renamed to 'room_chat'

--- services/backend/app/api/models/chat_message.py
+++ services/backend/app/api/models/chat_message.py
@@ -7,7 +7,7 @@ class ChatMessage(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     from_user_id = db.Column(db.Integer, db.ForeignKey('user.id'),
                              nullable=False)  # Many messages from one user
-    chat_room_id = db.Column(db.Integer, db.ForeignKey('chat_room.id'),
+    chat_room_id = db.Column(db.Integer, db.ForeignKey('room_chat.id'),
                            nullable=False)  # Many masseges to one chat room
     message = db.Column('message', db.String(255), nullable=False)
     timestamp = db.Column('timestamp', db.DateTime, nullable=False, default=db.func.current_timestamp())
