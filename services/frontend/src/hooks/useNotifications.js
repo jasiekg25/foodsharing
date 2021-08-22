bf8c5fcef85f268cb2535d9ffbf6f8377392b404
Mateusz <mmastalerczyk@gmail.com>
2021-08-22 21:24:17 +0100

    add notifications concatenation

--- services/frontend/src/hooks/useNotifications.js
+++ services/frontend/src/hooks/useNotifications.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useCallback } from "react";
 import io from "socket.io-client";
+import api from "../api";
 
 const accessToken = localStorage.getItem("accessToken");
 let socket = io.connect(`http://localhost:5001/notifs`);
@@ -7,21 +8,30 @@ let socket = io.connect(`http://localhost:5001/notifs`);
 export const useNotifications = () => {
   const [notifications, setNotifications] = useState([]);
   const [unreadCount, setUnreadCount] = useState(0);
-
+  
   const getMessages = useCallback((msg) => {
     const { notification } = msg;
     setNotifications([notification, ...notifications]);
     setUnreadCount(unreadCount + 1);
   });
 
+
   const clearUnreadCount = useCallback(() => {
     setUnreadCount(0);
   });
 
   useEffect(() => {
+    if (notifications.length == 0){
+      api.getUserNotifications()
+      .then((res) => {
+        setNotifications(res.data.map(notif => {return {"url": `/user_${notif.user_id}` , "message": notif.message};}))
+      })
+      .catch((err) => {
+          console.log(err);
+      })
+    }
     socket.emit("auth", { accessToken });
     socket.on("notification", getMessages);
-
     return () => {
       socket.off("notification", getMessages)
     }
