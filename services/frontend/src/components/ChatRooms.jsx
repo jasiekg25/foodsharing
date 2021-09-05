bcb34bf5074b7d00a94f6b92db082e758eb3b38d
Kinga <k.wierchomska@gmail.com>
2021-09-05 12:03:48 +0200

    feature/FOODSHARIN-144 chat improvements

--- services/frontend/src/components/ChatRooms.jsx
+++ services/frontend/src/components/ChatRooms.jsx
@@ -8,10 +8,26 @@ import {history} from "../index";
 function ChatRooms() {
 
     const [chatRooms, setChatRooms] = useState([]);
+    const [userId, setUserId] = useState({});
+
 
     useEffect(() => {
+        api.getProfile()
+            .then((res) => setUserId(res.data.id))
+            .catch((err) => {
+                toast.error("Could not get user information.")
+            })
         api.getUserChatRooms()
-            .then((res) => setChatRooms(res.data))
+            .then((res) => {
+                res.data.map(room => {
+                    if(room.last_message.length > 0){
+                        room.message = room.last_message[0].message
+                        room.messageTimestamp = room.last_message[0].timestamp
+                    }
+                })
+                setChatRooms(res.data)
+                console.log(res.data)
+            })
             .catch((err) => {
                 toast.error("Could not get any chats.")
             })
@@ -30,8 +46,9 @@ function ChatRooms() {
                             onClick={(e) => handleShowChat(chat.id, chat.offer_id)}
                             avatar={chat.offer_photo}
                             title={chat.offer_name}
-                            subtitle={chat.offer_owner_name}
-                            date={new Date()}
+                            subtitle= {chat.message}
+                            text= {chat.message}
+                            date={new Date(chat.messageTimestamp)}
                              />
                     </div>
                     )
